999b17916d20eccbf816cde25a6f1e36e2bcbd39
jainharshit27 <82932932+jainharshit27@users.noreply.github.com>
2021-08-22 10:23:51 +0530

    Add files via upload

---
+++ P13_S.py
@@ -0,0 +1,10 @@
+def tuple_avg(a_tuple):
+    count = 0
+    total = 0
+    for i in a_tuple:
+        print(a_tuple[count])
+        count += 1
+        total += i
+    print("Average:", total/count)
+
+tuple_avg((45,453,24,4,534,6,534,234324,32423,4,324,3,454541,1))
